1eb386319480b5c3d7ee0244e18a400954d7545e
Randy Bartels <rjbartels@outlook.com>
2024-12-17 18:10:45 -0500

    Shannon Entropy
    Signed-off-by: Randy Bartels <rjbartels@outlook.com>

---
+++ README_Shannon_Entropy.md
@@ -0,0 +1,57 @@
+# Shannon Entropy Calculator
+
+## Description
+I wrote this utility because I needed a way to find embedded-but-random content in application source code.  Maybe a hard-coded X.509 certificate or an AES-256 key.  Stuff like that.
+
+## Features
+It computes Shannon Entropy (as in Claude Shannon) on bytes.  
+* If run naked, it will ask for user input.  Human language (or at least English) will produce a Shannon Entropy of somewhere in the low-to-mid 2's.
+* It will also read a file - binary, text, doesn't matter.  In that case, there are more options available to tune the analysis.  Notably:
+    * Block Size -- How much data to process at once.  That is, how big is the buffer.
+    * Window Offset -- How much to move the buffer each time.
+* Write a CSV file with the results.
+    * Try charting the 'chunk_count' and 'shannon_entropy' fields as a line chart in Excel
+    * You should be able to spot outliers
+    * Add the noted chunks to a "review file" and use the tool again to output just those blocks to the screen
+
+
+## A Note About Shannon Entropy
+Entropy is really the measure of randomness -- or an indication of the "unknowable."  Try searching YouTube for "Shannon Entropy" and watch a couple of videos.
+
+With the default block size of 32 bytes, the "Ideal Entropy" is 5 (2^^5 = 32).
+
+On that note, from my own testing using some PowerShell scripts, /dev/random output, and Python source code anthing over about a 4.3 is interesting and north of 4.5 is probably completely random.  In that case, you've probably found a digital signature, a crypto key or some other interesting content.
+
+## Installation
+1. Clone the repository: `git clone https://github.com/flyguy62n/entropy_tools.git`
+2. Navigate to the project directory: `cd entropy_tools`
+
+## Usage
+1. Run the Shannon Entropy tool: `python shannon_entropy.py --help`
+
+### Analysis and Review
+Try this:
+1. `python shannon-entropy.py --in-file <path/to/file> --out-file <path/to/file>.csv`
+2. Review the CSV file in Excel.  Plot a chart using `chunk_num` and `shannon_entropy`
+3. Take note of any interesting spikes
+4. Add to the `chunk_num` to `review_blocks.txt` (one entry per line)
+5. `python shannon-entropy.py --in-file <path/to/file> --review-file review_blocks.txt`
+6. Confirm / reject the suspicious content
+7. Revel in your new-found ability to find embedded, random text
+
+Notes:
+* Depending on the nature of the secrets you're looking for, you might need to tune the searches
+* X.509 certificates are pretty long, so the defaults are probably fine.
+* AES Encryption Keys are comparitively shorter, so consider:
+    * The default `--block-size` of 32 bytes is probably fine since for AES-256 keys (that's 32 bytes)
+    * Consider reducing the `--window-offset` to 8, 4, or even 1 byte.  This is because if you go barreling through the file, you could accidentally split the AES key into 2 parts, which might "hide" it amongst the other content and you'd miss the spike.  With a smaller `--window-offset`, you'll get fine resolution and be more likely (absolutely certain if it's 1 byte) to capture the entire key in one block.
+    * In this scenario, you'll see the entropy ramp up and then back down again (bell-shaped curve) when you chart it in Excel
+
+## Contributing
+Contributions are welcome! If you have any suggestions or improvements, please open an issue or submit a pull request.
+
+## License
+This project is licensed under the [MIT License](LICENSE).
+
+## Contact
+For any questions or inquiries, please contact me.
---
+++ shannon_entropy.py
@@ -0,0 +1,174 @@
+#!/usr/bin/python3
+
+import argparse
+import csv
+import math
+import sys
+import textwrap
+
+from functools import partial
+from pathlib import Path
+
+
+def commandLineParser() -> argparse.ArgumentParser:
+    '''Handles the command line options'''
+    parser = argparse.ArgumentParser(
+    prog=sys.argv[0].split('/')[-1],
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description=textwrap.dedent('''
+        Calculate the Shannon entropy of a string or file
+
+        Calculating Entropy:
+            * A file can provided containing the values.  Entropy will be computed for blocks of the file.  See INPUT section below.
+            * The file will be read as binary data.
+            * If no file is provided, the program will ask the user for input.
+                                
+        Reviewing Results:
+            * Use the CSV output to plot the entropy of each block.
+            * Take note of interesting blocks and add them (one per line) to a text file.
+            * Use the "-r | --review-file" option.
+            * The content of those blocks will be output to the screen.
+            * Be sure to use the same Window Offset and Block Size as the original analysis or else the content won't line up.
+        
+        ''')
+    )
+
+    inputControl = parser.add_argument_group(title='Input')
+    inputControl.add_argument(
+        '-if', '--in-file',
+        dest='inputFile',
+        default='',
+        help='File to read the values from',
+    )
+    inputControl = parser.add_argument(
+        '-b', '--block-size',
+        dest='blockSize',
+        type=int,
+        default=32,
+        help='Size of each block to read from the file',
+    )
+    inputControl = parser.add_argument(
+        '-w', '--window-offset',
+        dest='windowOffset',
+        type=int,
+        default=32,
+        help='Offset to move the read window by each iteration'
+    )
+
+    outputControl = parser.add_argument_group(title='Output')
+    outputControl.add_argument(
+        '-of', '--out-file',
+        dest='outputFile',
+        default='',
+        help='CSV file to write the results to',
+    )
+    outputControl = parser.add_argument(
+        '-r', '--review-file',
+        dest='reviewFile',
+        default='',
+        help='A text file containing a list (one per line) of blocks to print to the screen',
+    )
+    outputControl = parser.add_argument(
+        '-v', '--verbose',
+        dest='verbose',
+        action='count',
+        default=0,
+        help='Print each block''s entropy to the screen',
+    )
+    
+    return parser.parse_args()
+
+# Entopy calcs scraped shamelessly from https://stackoverflow.com/questions/2979174/how-do-i-compute-the-approximate-entropy-of-a-bit-string
+# The rest is orginal to FlyGuy62n
+
+def entropy(string) -> float:
+    "Calculates the Shannon entropy of a string"
+
+    # get probability of chars in string
+    prob = [ float(string.count(c)) / len(string) for c in dict.fromkeys(list(string)) ]
+
+
+    # calculate the entropy
+    entropy = - sum([ p * math.log(p) / math.log(2.0) for p in prob ])
+
+    return entropy
+
+
+def entropy_ideal(length):
+    "Calculates the ideal Shannon entropy of a string with given length"
+
+    prob = 1.0 / length
+
+    return -1.0 * length * prob * math.log(prob) / math.log(2.0)
+
+
+def main():
+    args=commandLineParser()
+    print(args)
+
+    if args.reviewFile:
+        print(f'Reviewing results using {args.reviewFile}')
+        # Process the review file contents to produce a list of interesting blocks
+        try:
+            with open(args.reviewFile, 'r') as reviewFile:
+                try:
+                    interestingBlocks = [int(line.strip()) for line in reviewFile.readlines()]
+                except ValueError:
+                    print('Review file includes non-numeric content.')
+                    sys.exit(1)
+        except FileNotFoundError:
+            print(f'Review file not found: {args.reviewFile}')
+            sys.exit(1)
+        
+            
+        # Read through the file to find the interesting blocks
+        chunk_count = 0
+        with open(args.inputFile, 'rb') as inFile:
+            for block in iter(partial(inFile.read, args.blockSize), b''):
+                if chunk_count in interestingBlocks:
+                    print(f'Block {chunk_count}: {block}')
+                chunk_count += args.windowOffset
+                inFile.seek(chunk_count, 0)
+        sys.exit(0)
+    if args.inputFile:
+        chunk_count = 0
+        results = []
+        with open(args.inputFile, 'rb') as inFile:
+            for block in iter(partial(inFile.read, args.blockSize), b''):
+                # If we've reached the end of the file, we'll have a short block and go ahead and stop
+                if len(block) < args.blockSize:
+                    break
+                result = entropy(block)
+                if args.verbose:
+                    print (f'Chunk {chunk_count} entropy ({len(block)} bytes): {result} / {entropy_ideal(len(block))}')
+                results.append({
+                    'filename': args.inputFile,
+                    'chunk_num': chunk_count,
+                    'shannon_entropy': result,
+                    'ideal_entropy': entropy_ideal(len(block)),
+                    'block_size': len(block),
+                })
+                chunk_count += args.windowOffset
+                inFile.seek(chunk_count, 0)
+        if args.outputFile:
+            with open(args.outputFile, 'w', newline='') as csvfile:
+                fieldNames=['filename', 'chunk_num', 'shannon_entropy', 'ideal_entropy', 'block_size']
+                writer = csv.DictWriter(csvfile, fieldnames=fieldNames)
+                writer.writeheader()
+                for result in results:
+                    writer.writerow(result)
+            print(f'Results written to {args.outputFile}')
+    else:    
+        while True:
+            try:
+                s = input('Enter a string (or CTRL-C to quit): ')
+            except KeyboardInterrupt:
+                sys.exit()
+
+            print(f'String length  : {len(s)}')
+            print(f'Shannon entropy: {entropy(s)}')
+            print(f'Ideal entropy  : {entropy_ideal(len(s))}')
+        
+    
+if __name__=='__main__':
+    main()
