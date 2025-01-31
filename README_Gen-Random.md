ef8cadc84278e25b0d1f6272db6e489dfddfccd0
Randy Bartels <rjbartels@outlook.com>
2025-01-31 15:15:56 -0500

    Generate Random Data
    Signed-off-by: Randy Bartels <rjbartels@outlook.com>

---
+++ README_Gen-Random.md
@@ -0,0 +1,38 @@
+# Generate Random Data
+
+## Description
+I wrote this utility because I needed a way to create random data that I could use to become familiar with the [NIST Statistical Test Suite](https://github.com/arcetri/sts) as part of a project.
+
+## Features
+It generates random data using the Python `os.urandom()` method, which should in turn get its random data from the underlying OS.
+* If run naked, it will generate one file of 4,096 x 32-byte random binary data and write the result to `bin.out`.  You should recognize 32-bytes as the equivalent of AES-256 keys.
+* Other options have been added to support specific project requirements that have arisen.  For instance:
+    * An option to generate a SHA-256 has as the random data that is based on a random 32-byte key PLUS the unique Machine GUID, as a customer was using this method to generate their encryption keys and I wanted to see if it hurt the randomness of the key.
+    * Specify the number of random files to create as well as the size of the blocks and the number of blocks in each file.
+
+Use `gen-random.py --help` to see all of the available options.
+
+## Installation
+1. Clone the repository: `git clone https://github.com/flyguy62n/entropy_tools.git`
+2. Navigate to the project directory: `cd entropy_tools`
+3. (Optional) Create a Python virtual environemt: `python3 -m venv .venv`
+4. (OPtional) Activate the VENV: (Linux/MacOS) `source .venv/bin/activate` or (Windows PS) `.venv/Scripts/activate.ps1`
+5. Install the requirements: `pip install -r requirements-gen_random.txt`
+
+## Usage
+1. Activate the VENV (if not already activated): (Linux/MacOS) `source .venv/bin/activate` or (Windows PS) `.venv/Scripts/activate.ps1`
+2. Run the tool: `python gen_random.py --help`
+3. Deactive the VENV when done (or just close the shell prompt): `deactivate`
+
+### Analysis and Review
+1. Use the NIST STS (such as available [here](https://github.com/arcetri/sts)) to parse the binary file: `sts bin.out`
+2. Review the `result.txt` file
+
+## Contributing
+Contributions are welcome! If you have any suggestions or improvements, please open an issue or submit a pull request.
+
+## License
+This project is licensed under the [MIT License](LICENSE).
+
+## Contact
+For any questions or inquiries, please contact me.
--- gen_random.py
+++ gen_random.py
@@ -1,8 +1,17 @@
 #!/bin/env python3
 
 import argparse
+import hashlib
 import os
 import sys
+
+try:
+    import machineid 
+except ModuleNotFoundError:
+    print ('py-machineid module not found.  Do you need to activate a VENV or maybe install it with:\n')
+    print ('\tpip install -r requirements-gen_random.txt')
+    sys.exit(1)
+
 
 def commandLineParser() -> argparse.ArgumentParser:
     '''Handles the command line options'''
@@ -26,11 +35,24 @@ def commandLineParser() -> argparse.ArgumentParser:
         help='Size of an individual block (def=32, e.g. for AES-256)',
         )
     configControl.add_argument(
-        '-n', '--num-blocks',
+        '-b', '--blocks',
         dest='num_blocks',
         default='4096',
         type=int,
         help='Number of blocks to create (def=4096)',
+    )
+    configControl.add_argument(
+        '-g', '--machine-guid',
+        dest='machine_id',
+        action='store_true',
+        help='Use the Machine ID as a pepper along with SHA-256 when generating keys',
+    )
+    configControl.add_argument(
+        '-n', '--num-files',
+        dest='num_files',
+        type=int,
+        default=1,
+        help='Number of files to create',
     )
     configControl.add_argument(
         '-f', '--out-file',
@@ -42,21 +64,33 @@ def commandLineParser() -> argparse.ArgumentParser:
         '-v', '--verbose',
         dest='verbose',
         action='store_true',
-        help='Print byte content to STDOUT as well'
-
+        help='Print byte content to STDOUT as well',
     )
     
     return parser.parse_args()
 
 def main():
     args=commandLineParser()
-    with open(args.filename, "wb") as f:
-        for i in range(args.num_blocks):
-            key = os.urandom(args.blocksize)
-            f.write(key)
-            if args.verbose:
-                print(f'Index [{i+1}]: {str(key)}')
-    print(f'Content written to {args.filename}')
+    print(args)
+    if args.machine_id:
+        print('Using Machine ID to pepper the key for SHA-256')
+        m_id=bytes(machineid.id(), 'utf-8')
+    for file_count in range (args.num_files):
+        if args.filename.find('.') > -1:                                # If the file name includes a period
+            filename_parts = args.filename.split('.')
+            filename_parts[-2] = f'{filename_parts[-2]}_{file_count}'   # Append an _# to the second-to-last part of the file (the last part before the final .ext)
+            filename = '.'.join(filename_parts)                         # Re-join the file parts with dots
+        else:
+            filename = f'{args.filename}_{file_count}'
+        with open(filename, "wb") as f:
+            for i in range(args.num_blocks):
+                key = os.urandom(args.blocksize)
+                if args.machine_id:
+                    key=hashlib.sha256(m_id+key).digest()               # Use a SHA-256 of the random key and the Machine GUID as the key
+                f.write(key)
+                if args.verbose:
+                    print(f'Index [{i+1}]: {str(key)}')
+        print(f'Content written to {filename}')
 
 if __name__ == "__main__":
     main()
